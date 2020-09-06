941720ec6d54702e7fa2a00eee93d3c597671426
yijie4188 <yijie4188@gmail.com>
2020-09-07 01:48:48 +0800

    优化路由获取

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,28 @@
+/**
+ * @desc      项目总路由文件 index.js
+ * @author    yijie
+ * @date      2020-09-05
+ * @logs[0]   yijie 2020-09-05 创建了index.js文件
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+let modulesRoutes = []
 if (process.env.NODE_ENV === '') {
 } else if (process.env.NODE_ENV === 'production') {
+  modulesRoutes = require('../config.pro.js').routes
 } else if (process.env.NODE_ENV === 'development') {
   Vue.use(VueRouter)
+  modulesRoutes = require('../config.dev.js').routes
 } else {
 }
 
-const Home = _ => import(
-  /* webchunkName: "Home" */
-  '../views/Home.vue'
-)
-const About = _ => import(
-  /* webchunkName: "About" */
-  '../views/About.vue'
-)
+const baseRoutes = []
 
-const routes = [{
-  path: '/',
-  name: 'Home',
-  component: Home
-}, {
-  path: '/about',
-  name: 'About',
-  component: About
-}]
+const routes = [
+  ...modulesRoutes,
+  ...baseRoutes
+]
 
 const router = new VueRouter({
   mode: 'history',
