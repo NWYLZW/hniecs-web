b768bd5777d08731386cf6aef20d259e2d7dd498
yijie4188 <yijie4188@gmail.com>
2020-09-07 07:59:07 +0800

    添加个人中心内容

--- src/components/topNavigationBar.vue
+++ src/components/topNavigationBar.vue
@@ -27,23 +27,70 @@
       </div>
     </div>
     <div class="right">
-      <div class="avatar">
-        <el-avatar
+      <el-popover class="avatar"
+        placement="bottom"
+        width="300"
+        title="个人中心" trigger="hover">
+        <el-avatar slot="reference"
           :size="48"
           src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"/>
-      </div>
+        <div class="user-center-panel">
+          <div class="labels">
+            <div class="label">
+              <div class="name">基础信息</div>
+              <div class="content">
+                当前免费积分 20'，当前可用积分 10'。
+              </div>
+            </div>
+          </div>
+          <div class="labels">
+            <div class="label" style="max-width: 50%;">
+              <div class="name">磁盘空间</div>
+              <el-progress
+                type="circle" width="100"
+                :percentage="1" status="success"></el-progress>
+            </div>
+            <div class="label" style="max-width: 50%;">
+              <div class="name">磁盘信息</div>
+              <div class="content">磁盘总大小 100M, 目前已使用 1M。</div>
+            </div>
+          </div>
+          <div style="clear: both;"></div>
+        </div>
+      </el-popover>
       <div class="apps">
-        <el-tooltip
-          :key="index"
-          v-for="(app, index) in apps"
-          :content="app.name" placement="top">
-        <span
-          class="app hniecs-iconfont"
-          :style="{
-            color: app.color || 'auto'
-          }"
-          v-html="app.iconUTF8"/>
-        </el-tooltip>
+        <div class="app"
+             :key="index"
+             v-for="(app, index) in apps">
+          <el-tooltip v-if="!app.menus"
+            :content="app.name" placement="bottom">
+            <span
+              class="hniecs-iconfont"
+              :style="{
+                color: app.color || 'auto'
+              }" v-html="app.iconUTF8"/>
+          </el-tooltip>
+          <el-dropdown placement="bottom" v-else>
+            <span
+              class="hniecs-iconfont"
+              :style="{
+                color: app.color || 'auto'
+              }" v-html="app.iconUTF8"/>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item
+                :key="index"
+                v-for="(menuItem, index) in app.menus">
+                <span
+                  style="float: left;margin-right: 10px;"
+                  class="hniecs-iconfont"
+                  :style="{
+                    color: menuItem.color || 'auto'
+                  }" v-html="menuItem.iconUTF8"/>
+                <span>{{ menuItem.name }}</span>
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
       </div>
     </div>
   </div>
@@ -73,6 +120,19 @@ export default {
         return [{
           name: '后台管理',
           iconUTF8: '&#xe76e;'
+        }, {
+          name: '我的消息',
+          iconUTF8: '&#xe6a2;',
+          menus: [{
+            name: '公告',
+            iconUTF8: '&#xe6aa;'
+          }, {
+            name: '聊天',
+            iconUTF8: '&#xe6a9;'
+          }, {
+            name: '看板',
+            iconUTF8: '&#xe6ab;'
+          }]
         }]
       }
     }
@@ -111,6 +171,11 @@ export default {
 }
 .pointer {
   cursor: pointer;
+}
+
+.hniecs-iconfont {
+  font-size: 32px;
+  color: map-get($use-colors, font-color);
 }
 
 .main {
@@ -204,8 +269,17 @@ export default {
 
       > .app {
         @extend .pointer;
-        color: map-get($use-colors, font-color);
-        margin: 10px;
+        position: relative; float: left;
+
+        height: 65px;
+        .el-dropdown {
+          height: 65px;
+        }
+
+        margin: {
+          left: 10px;
+          right: 10px;
+        };
       }
     }
     > .avatar {
@@ -217,6 +291,28 @@ export default {
       /deep/ img {
         @extend .pointer;
         width: 100%;height: 100%;
+      }
+    }
+  }
+}
+
+.user-center-panel {
+  padding: 20px;
+  .labels {
+    position: relative;float: left;
+    width: 100%;
+    margin-bottom: 20px;
+
+    > .label {
+      position: relative;
+      float: left;
+
+      > .name {
+        font-size: 20px;
+        font-weight: 600;
+      }
+
+      > .content {
       }
     }
   }
