d5dfa7b0908135a8b8263369d9ea6ca3399346a4
yijie4188 <yijie4188@gmail.com>
2020-09-07 17:28:17 +0800

    首页添加轮播图

--- src/modules/door/views/index/Home.vue
+++ src/modules/door/views/index/Home.vue
@@ -6,11 +6,35 @@
 -->
 <template>
   <div class="main">
-    主页面
+    <el-carousel
+      class="top-carousel"
+      :interval="5000" type="card" height="300px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="medium">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script src="../../assets/js/index/home.js"></script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "~@/modules/door/assets/style/home.scss";
+/deep/ .el-carousel__item {
+  transition: .3s;
+  &:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  &:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+  h3 {
+    position: relative;
+    bottom: calc(-100% + 40px);left: 20px;
+    color: #475669;
+    text-align: left;
+    font-size: 20px;
+    margin: 0;
+  }
+}
 </style>
