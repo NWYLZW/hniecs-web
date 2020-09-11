b05a2331ceefcb3785b109636fb34a1e38cc7d37
yijie4188 <yijie4188@gmail.com>
2020-09-12 02:32:21 +0800

    修改图片位置

--- src/modules/door/views/user/Login.vue
+++ src/modules/door/views/user/Login.vue
@@ -132,6 +132,7 @@ export default {
   position: absolute;
   width: 100%; height: 100%;
 
+  background-position: center;
   background-image: url("#{$base-path}/static/images/door/login-background-image-01.jpg");
 
   .login-card {
