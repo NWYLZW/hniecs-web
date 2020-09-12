7eae0841eea72cb28fdd75a65766006647ea2e7c
yijie4188 <yijie4188@gmail.com>
2020-09-12 22:36:03 +0800

    添加登陆接口

--- src/modules/door/assets/js/rpc.js
+++ src/modules/door/assets/js/rpc.js
@@ -4,8 +4,14 @@
  * @date      2020-09-07
  * @logs[0]   yijie 2020-09-07 创建了rpc.js文件
  */
+import axios from '@plugins/axios'
 // mock拦截ajax请求 伪造假数据
 // import '@mock'
 
 export default {
+  user: {
+    login () {
+      return axios.get()
+    }
+  }
 }
