d6403f330e7ce6268ea624c118e79173aa65551b
yijie4188 <yijie4188@gmail.com>
2020-09-18 16:36:53 +0800

    修改了导入路由页面的方式

--- src/modules/admin/assets/js/mannage/router.js
+++ src/modules/admin/assets/js/mannage/router.js
@@ -1,17 +1,21 @@
 /**
- * @desc      管理模块路由文件 router.js
- * @author    yijie
- * @date      2020-09-08 00:00
- * @logs[0]   yijie 2020-09-08 00:00 创建了文件router.js
+ * @desc    管理模块路由文件 router.js
+ * @author  yijie
+ * @date    2020-09-18 15:30
+ * @logs[0] 2020-09-18 15:30 yijie 创建了文件router.js
+ * @logs[1] 2020-09-18 16:35 yijie 修改了导入路由页面的方式
  */
+
+const impoartPageView = view => () =>
+  import(
+    /* webChunkName: "admin-manage" */
+    '@modules/admin/views/manage/' + view + '.vue'
+  )
 
 export default [{
   path: '/admin/manage/index',
   name: 'admin-manage-index',
-  component: _ => import(
-    /* webChunkName: "admin-manage" */
-    '@modules/admin/views/manage/Index.vue'
-  ),
+  component: impoartPageView('Index'),
   meta: {
     title: '后台管理',
     need: { admin: true }
@@ -19,10 +23,7 @@ export default [{
   children: [{
     path: '/admin/manage/user',
     name: 'admin-manage-user',
-    component: _ => import(
-      /* webChunkName: "admin-manage" */
-      '@modules/admin/views/manage/User.vue'
-    ),
+    component: impoartPageView('User'),
     meta: {
       title: '用户管理',
       need: { admin: true }
