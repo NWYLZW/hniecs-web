c03985f4451c0bf5a0210cc747dbdd2ba62af730
yijie4188 <yijie4188@gmail.com>
2020-09-26 22:36:38 +0800

    [feat] (tool) 引入装饰器

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    // vue-property-decorator 配置
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }]
   ]
 }
