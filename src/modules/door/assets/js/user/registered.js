5ba527291d9637e816d0ad61d32c0e0d974ac324
yijie4188 <yijie4188@gmail.com>
2020-09-30 20:20:38 +0800

    [feat] (page) 注册界面

--- src/modules/door/assets/js/user/registered.js
+++ src/modules/door/assets/js/user/registered.js
@@ -5,5 +5,62 @@
  * @logs[0] 2020-09-30 10:47 yijie 创建了registered.js文件
  */
 export default {
-  name: 'registered'
+  name: 'registered',
+  data () {
+    return {
+      // 背景图列表
+      backgroundImages: [
+        process.env.BASE_URL + 'static/images/door/backgroundImages/01.png',
+        process.env.BASE_URL + 'static/images/door/backgroundImages/02.png',
+        process.env.BASE_URL + 'static/images/door/backgroundImages/04.png'
+      ],
+      // 当前正在填写的表单序号
+      curPageIndex: 1,
+      // 表单字典
+      form: {
+        // 用户名
+        userName: '',
+        // 密码
+        password: '',
+        // 确认密码
+        confirmPassword: '',
+        // 真实姓名
+        realName: '',
+        // 学号
+        schoolNum: '',
+        // 专业
+        profession: '',
+        // 班级
+        classNum: '',
+        // qq号码
+        qqNum: '',
+        // 电话号码
+        telNum: '',
+        // 邀请码
+        invitationCode: ''
+      }
+    }
+  },
+  methods: {
+    /**
+     * 点击了底部的按钮
+     * @param clickIndex  按钮的序号
+     */
+    clickBottomBtn (clickIndex) {
+      switch (clickIndex) {
+        case 1:
+          // 上一步
+          this.curPageIndex -= 1
+          break
+        case 2:
+          // 下一步
+          this.curPageIndex += 1
+          break
+        case 3:
+          // 提交
+          break
+        default: break
+      }
+    }
+  }
 }
--- src/modules/door/views/user/Registered.vue
+++ src/modules/door/views/user/Registered.vue
@@ -6,11 +6,100 @@
 -->
 
 <template>
-  <div>hello register</div>
+  <div class="main" :style="'background-image: url(' + backgroundImages[curPageIndex-1] + ')'">
+    <div class="registered-card">
+      <div class="header">
+        <img class="icon-image" src="@assets/image/logo.svg" alt="None">
+        <div class="steps">
+          <div
+            :key="index"
+            v-for="index in 3"
+            class="step" :class="{
+              'pass': index < curPageIndex,
+              'sel': index === curPageIndex
+            }"/>
+        </div>
+      </div>
+      <el-form ref="registered-form" :model="form">
+        <div
+          class="content" :class="{
+            'show': curPageIndex === 1
+          }">
+          <el-form-item label="用户名"
+                        prop="userName">
+            <el-input v-model="form.userName"/>
+          </el-form-item>
+          <el-form-item label="密码"
+                        prop="password">
+            <el-input v-model="form.password" show-password/>
+          </el-form-item>
+          <el-form-item label="确认密码"
+                        prop="confirmPassword">
+            <el-input v-model="form.confirmPassword" show-password/>
+          </el-form-item>
+        </div>
+        <div
+          class="content" :class="{
+            'show': curPageIndex === 2
+          }">
+          <el-form-item label="真实姓名"
+                        prop="realName">
+            <el-input v-model="form.realName"/>
+          </el-form-item>
+          <el-form-item label="学号"
+                        prop="realName">
+            <el-input v-model="form.schoolNum"/>
+          </el-form-item>
+          <el-form-item label="专业"
+                        prop="realName">
+            <el-input v-model="form.profession"/>
+          </el-form-item>
+          <el-form-item label="班级"
+                        prop="realName">
+            <el-input v-model="form.classNum"/>
+          </el-form-item>
+        </div>
+        <div
+          class="content" :class="{
+            'show': curPageIndex === 3
+          }">
+          <el-form-item label="联系QQ"
+                        prop="qqNum">
+            <el-input v-model="form.qqNum"/>
+          </el-form-item>
+          <el-form-item label="联系电话"
+                        prop="telNum">
+            <el-input v-model="form.telNum"/>
+          </el-form-item>
+          <el-form-item label="邀请码"
+                        prop="invitationCode">
+            <el-input v-model="form.invitationCode"/>
+          </el-form-item>
+        </div>
+        <div class="bottom-btns">
+          <el-button @click="clickBottomBtn(1)" type="primary" v-show="curPageIndex !== 1">上一步</el-button>
+          <el-button @click="clickBottomBtn(2)" type="primary" v-if="curPageIndex !== 3">下一步</el-button>
+          <el-button @click="clickBottomBtn(3)" type="primary" v-else>提交</el-button>
+        </div>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script src="../../assets/js/user/registered.js"></script>
 
 <style lang="scss" scoped>
 @import "~@/modules/door/assets/style/registered.scss";
+.content /deep/ .el-form-item {
+  margin-bottom: 10px;
+  .el-form-item__label {
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+    text-shadow: 0 0 10px gray;
+  }
+  .el-input__inner {
+    background-color: rgba(255, 255, 255, .5);
+  }
+}
 </style>
